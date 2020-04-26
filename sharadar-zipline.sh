2488197c07b99f5852e6808959a8d856fde23b38
Costantino <c.cerbo@gmail.com>
2020-04-26 20:27:29 +0200

    sharadar-zipline.sh entry script

---
+++ sharadar-zipline.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Examples:
+#   sharadar-zipline.sh ingest -b sharadar
+#   sharadar-zipline.sh run -b sharadar -f examples/momentum_pipeline.py -s 2020-01-01 -e 2020-02-01
+
+python zipline-main.py "$@"
