0edf71f05cd8c5472b5d0421729b06210610df10
Costantino <c.cerbo@gmail.com>
2020-04-29 19:18:02 +0000

    setup fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='sharadar-zipline',
+    name='sharadar_db_bundle',
     version='1.0',
     packages=['sharadar', 'sharadar.data', 'sharadar.loaders', 'sharadar.pipeline', 'sharadar.util'],
     url='',
