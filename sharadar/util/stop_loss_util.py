d96bfba6ef5369620cc378f7784f1d9558f17588
Costantino <c.cerbo@gmail.com>
2020-08-11 10:01:31 +0200

    stop_loss_util: portfolios and single assets

--- sharadar/util/stop_loss_util.py
+++ sharadar/util/stop_loss_util.py
@@ -1,19 +1,30 @@
 from zipline.api import get_datetime
 from zipline.api import order_target
 
-
 def stop_loss_portfolio(context, data, log, close_all):
     """
     Close all positions when the whole portfolio loss exceeded the loss_limit
     """
-    if context.portfolio_value_month_start is not None:
-        montly_return = context.portfolio.portfolio_value / context.portfolio_value_month_start - 1.0
-        if montly_return <= context.PARAM['loss_limit']:
-            log.warn("Montly loss (%.2f) exceeded the loss limit: close all positions." % (100.0*montly_return))
-            close_all(context, data)
-            # To avoid repeated triggers
-            context.portfolio_value_month_start = None
-            context.INFO['stop_loss_date'].append(get_datetime())
+
+    positions = context.portfolio.positions
+    if len(positions) == 0:
+        return
+
+    initial_value = 0
+    last_value = 0
+
+    for stock, position in list(positions.items()):
+        cost_basis = position.cost_basis
+        if cost_basis == 0:
+            cost_basis = position.last_sale_price
+
+        initial_value += cost_basis * position.amount
+        last_value += position.last_sale_price * position.amount
+
+    montly_return = last_value / initial_value - 1.0
+    if montly_return <= context.PARAM['loss_limit']:
+        log.warn("Montly loss (%.2f) exceeded the loss limit: close all positions." % (100.0*montly_return))
+        close_all(context, data)
 
 
 def stop_loss_equities(context, data, log):
