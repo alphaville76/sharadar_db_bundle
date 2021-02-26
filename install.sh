ee41ea02fc0a855868d90be00272c168f0fb9913
Costantino <c.cerbo@gmail.com>
2021-02-26 23:34:54 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,46 @@
+mkdir -p ~/dev/sharadar_db_bundle/
+cd ~/dev/sharadar_db_bundle/
+virtualenv -p /usr/bin/python3.6 venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+pip install pytest
+cd venv/lib/python3.6/site-packages/
+
+# Install TA LIB
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar xvfz ta-lib-0.4.0-src.tar.gz
+rm ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure
+make
+cd ..
+
+# Install zipline-trades and deps
+sudo dnf install -y libpq5-devel
+git clone https://github.com/shlomikushchi/zipline-trader.git
+cd zipline-trader
+pip install wheel
+pip install -r etc/requirements_build.in
+export DISABLE_BCOLZ_AVX2=true
+pip install --no-binary=bcolz -e .[all] -r etc/requirements_blaze.in
+cd ..
+
+# Install TWS api
+wget https://interactivebrokers.github.io/downloads/twsapi_macunix.976.01.zip
+unzip twsapi_macunix.976.01.zip -d twsapi
+cd twsapi/IBJts/source/pythonclient
+python setup.py install
+cd -
+
+pip install memoization singleton_decorator quandl
+git clone https://github.com/alphaville76/sharadar_db_bundle.git
+cd sharadar_db_bundle
+python setup.py install
+python test/basic_pipeline_sep_db.py
+
+if [ "$?" -eq 0 ]
+then
+echo "INSTALLATION SUCCESSFUL"
+else
+echo "INSTALLATION FAILED"
+fi
--- test/basic_pipeline_sep_db.py
+++ test/basic_pipeline_sep_db.py
@@ -9,16 +9,22 @@ import datetime
 
 bundle = load_sharadar_bundle()
 
-print(bundle.asset_finder.get_info([199059, 199623], 'sector'))
+sectors = bundle.asset_finder.get_info([199059, 199623], 'sector')
+print("sectors", sectors)
 
-bundle.asset_finder.retrieve_equities([199059, 199623])
-print(symbols(['SPY']))
+equities = bundle.asset_finder.retrieve_equities([199059, 199623])
+print("equities", equities)
+
+syms = symbols(['SPY'])
+print("symbols", syms)
+assert len(syms) == 1
+assert syms[0].exchange == 'NYSEARCA'
+assert syms[0].sid == 118691
 
 spe = make_pipeline_engine()
 
 pipe_start = pd.to_datetime('2020-02-03', utc=True)
 pipe_end = pd.to_datetime('2020-02-07', utc=True)
-
 
 pipe = Pipeline(columns={
     'Close': USEquityPricing.close.latest,
@@ -27,7 +33,8 @@ screen = StaticAssets(symbols(['IBM', 'F', 'AAPL']))
 )
 
 stocks = spe.run_pipeline(pipe, pipe_start, pipe_end)
-print("stocks.shape [close]", stocks)
+print("stocks.shape [close]", stocks.shape)
+assert stocks.shape == (15, 1)
 
 from sharadar.pipeline.factors import MarketCap, EV, Fundamentals
 pipe_mkt_cap = Pipeline(columns={
@@ -38,7 +45,7 @@ pipe_mkt_cap = Pipeline(columns={
 start_time = time.time()
 stocks = spe.run_pipeline(pipe_mkt_cap, pipe_start, pipe_end)
 print("stocks.shape [mkt cap]", stocks.shape)
-print("--- %s ---" % datetime.timedelta(seconds=(time.time() - start_time)))
+assert stocks.shape == (49857, 1)
 
 pipe_mkt_cap_ev = Pipeline(columns={
     'mkt_cap': MarketCap(),
@@ -50,6 +57,11 @@ screen = StaticAssets(symbols(['IBM', 'F', 'AAPL']))
 )
 
 stocks = spe.run_pipeline(pipe_mkt_cap_ev, pipe_start, pipe_end)
-print(stocks)
+print(stocks.shape)
+assert stocks.shape == (15, 4)
 
-
+print(stocks.iloc[0])
+assert stocks.iloc[0]['cash'] == 39771000000.0
+assert stocks.iloc[0]['debt'] == 108292000000.0
+assert stocks.iloc[0]['ev'] == 1422775814800.0
+assert stocks.iloc[0]['mkt_cap'] == 1354254814800.0
