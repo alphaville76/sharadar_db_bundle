5e010dfee16cedb6d878b43c88303d9b4085d2cd
Costantino <c.cerbo@gmail.com>
2021-02-27 20:39:51 +0100

    Update install.sh
    cd $PYTHON_LIBS

--- install.sh
+++ install.sh
@@ -4,18 +4,19 @@ virtualenv -p /usr/bin/python3.6 venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install pytest
-cd venv/lib/python3.6/site-packages/
+export PYTHON_LIBS=$(python -c "import sys;print(sys.path[-1])")
 
 # Install TA LIB
+cd $PYTHON_LIBS
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar xvfz ta-lib-0.4.0-src.tar.gz
 rm ta-lib-0.4.0-src.tar.gz
 cd ta-lib
 ./configure
 make
-cd ..
 
 # Install zipline-trades and deps
+cd $PYTHON_LIBS
 sudo dnf install -y libpq5-devel
 git clone https://github.com/shlomikushchi/zipline-trader.git
 cd zipline-trader
@@ -23,17 +24,17 @@ pip install wheel
 pip install -r etc/requirements_build.in
 export DISABLE_BCOLZ_AVX2=true
 pip install --no-binary=bcolz -e .[all] -r etc/requirements_blaze.in
-cd ..
 
 # Install TWS api
+cd $PYTHON_LIBS
 wget https://interactivebrokers.github.io/downloads/twsapi_macunix.976.01.zip
 unzip twsapi_macunix.976.01.zip -d twsapi
 cd twsapi/IBJts/source/pythonclient
 python setup.py install
-cd -
 
 pip install memoization singleton_decorator quandl pyfolio
 pip install pandas==0.22.0
+cd $PYTHON_LIBS
 git clone https://github.com/alphaville76/sharadar_db_bundle.git
 cd sharadar_db_bundle
 python setup.py install
