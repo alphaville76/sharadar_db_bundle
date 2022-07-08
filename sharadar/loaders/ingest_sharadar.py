34c7f7087371c86c6417d1641dc1cd59dd2abf62
Costantino <c.cerbo@gmail.com>
2022-07-08 20:26:02 +0200

    exchange-calendars>=3.6.3 Added Juneteenth as a public holiday in XCBF

--- sharadar/loaders/ingest_sharadar.py
+++ sharadar/loaders/ingest_sharadar.py
@@ -62,8 +62,8 @@ def fetch_data(start, end):
         df_sep = fetch_table_by_date(env["NASDAQ_API_KEY"], 'SHARADAR/SEP', start, end)
         df_sfp = fetch_table_by_date(env["NASDAQ_API_KEY"], 'SHARADAR/SFP', start, end)
 
-    #df = df_sep.append(df_sep)
-    df = pd.concat(df_sep, df_sep)
+    df = df_sep.append(df_sfp)
+    #df = df_sep.insert(len(df_sep), df_sfp)
     df = df.drop_duplicates().reset_index(drop=True)
     return df
 
