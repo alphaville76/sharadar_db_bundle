f9a1058cafa5066da2b7ce12b73984519ccd182f
Costantino <c.cerbo@gmail.com>
2022-08-29 21:04:52 +0200

    context white list

--- sharadar/util/serialization_utils.py
+++ sharadar/util/serialization_utils.py
@@ -1,16 +1,6 @@
 import pickle
 import json
 
-# Persistence blacklist/whitelist and excludes gives a way to include/
-# exclude (so do not persist on disk if initiated or excluded from the serialization
-# function that reinstate or save the context variable to its last state).
-# trading client can never be serialized, the initialized function and
-# perf tracker remember the context variables and the past performance
-# and need to be whitelisted
-_context_blacklist = ['trading_client']
-_context_whitelist = ['initialized', 'perf_tracker']
-
-
 def load_context(state_file_path, context):
     with open(state_file_path, 'rb') as f:
         loaded_state = pickle.load(f)
@@ -20,11 +10,8 @@ def load_context(state_file_path, context):
 
 
 def store_context(state_file_path, context):
-    exclude_list = _context_blacklist + [e for e in context.__dict__.keys() if e not in _context_whitelist]
-    fields_to_store = list(set(context.__dict__.keys()) - set(exclude_list))
-
     state = {}
-    for field in fields_to_store:
+    for field in context.whitelist:
         state[field] = getattr(context, field)
 
     with open(state_file_path, 'wb') as f:
@@ -34,7 +21,9 @@ def store_context(state_file_path, context):
 if __name__ == '__main__':
     import sys
 
-    context_file_path = sys.argv[1]
+    print("Enter file path:")
+    #context_file_path = str(input())
+    context_file_path = '/home/c.cerbo/zipline-reloaded-venv3.9/lib/python3.9/site-packages/sharadar_db_bundle/algo/cluster/clusters13g.state'
     with open(context_file_path, 'rb') as f:
         context = pickle.load(f)
 
